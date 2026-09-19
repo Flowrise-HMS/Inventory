7da3b49065b1667f975b127c8b548a5c0308797b
musheabdulhakim <musheabdulhakim99@gmail.com>
2026-09-19 19:36:50 +0000

    feat: place the inventory cluster in the Operations sidebar group

--- app/Filament/Clusters/Inventory/InventoryCluster.php
+++ app/Filament/Clusters/Inventory/InventoryCluster.php
@@ -5,12 +5,15 @@ namespace Modules\Inventory\Filament\Clusters\Inventory;
 use BackedEnum;
 use Filament\Clusters\Cluster;
 use Filament\Support\Icons\Heroicon;
+use Modules\Core\Enums\SidebarGroup;
 
 class InventoryCluster extends Cluster
 {
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedCube;
 
-    protected static string|\UnitEnum|null $navigationGroup = null;
+    protected static string|\UnitEnum|null $navigationGroup = SidebarGroup::Operations;
+
+    protected static ?int $navigationSort = 10;
 
     protected static ?string $navigationLabel = 'Inventory';
 }
